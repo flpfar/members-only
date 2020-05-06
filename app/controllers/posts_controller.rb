f632e05dc78ad78331a6dfbbf0bd4208ba69ba18
sergiomauz <sergio.mauz88@gmail.com>
2020-05-06 13:02:32 -0500

    Added post controller and user management

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,13 +2,25 @@ class PostsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
   
   def new
+    @post = current_user.posts.new
   end
 
   def create
+    @post = current_user.posts.new(post_params)
+    @post.save
 
+    redirect_to posts_path
   end
 
   def index
-
+    @posts = Post.all    
   end
+
+
+  private 
+
+    def post_params
+      params.require(:post).permit(:body)
+    end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -14,8 +14,10 @@ ActiveRecord::Schema.define(version: 2020_05_06_160508) do
 
   create_table "posts", force: :cascade do |t|
     t.text "body"
+    t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|

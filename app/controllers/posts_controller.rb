61f9931bf1cb547262a35d586b8927bc5bee8c96
Felipe Rosa <flp.far@gmail.com>
2020-05-06 13:04:37 -0300

    Add posts model, controller and routes

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,14 @@
+class PostsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
+  
+  def new
+  end
+
+  def create
+
+  end
+
+  def index
+
+  end
+end
